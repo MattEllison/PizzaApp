418fbb3c0b3bc9a4303062d2d5ed93005435ffde
MattEllison <matthewcellison@gmail.com>
2017-09-25 18:21:14 -0500

    first commit

---
+++ PizzaApp/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PizzaApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public static List<string> Toppings = new List<string>() { "Anchovies", "Pepperoni", "Olives","Bacon", "Tomatoes" };
+        public ActionResult Index(string ph)
+        {
+            // get phone number
+            
+            return View();
+        }
+        
+        
+       
+    }
+    
+
+   
+}
